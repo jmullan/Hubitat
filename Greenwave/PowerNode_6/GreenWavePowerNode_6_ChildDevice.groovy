67e92921a0e07920db61fb258cb6164663d06f8f
Jesse Mullan <jmullan@gmail.com>
2019-12-04 00:36:26 -0800

    Better exception handling, consistent 6 child device setup

--- Greenwave/PowerNode_6/GreenWavePowerNode_6_ChildDevice.groovy
+++ Greenwave/PowerNode_6/GreenWavePowerNode_6_ChildDevice.groovy
@@ -10,7 +10,7 @@
  *  Source and info: https://github.com/CopyCat73/SmartThings/tree/master/devicetypes/copycat73/greenwave-powernode-6-child-device.src
  *
  *  Author: Nick Veenstra
- *  Thanks to Eric Maycock for code inspiration 
+ *  Thanks to Eric Maycock for code inspiration
  *
  *  Description: Device handler for the GreenWave PowerNode (multi socket) Z-Wave power outlet child nodes
  *
@@ -24,64 +24,74 @@
  *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *   for the specific language governing permissions and limitations under the License.
  *
- *   30/08/2019 Updated by @Royski to work with Hubitat, with help from @chuck.schwer and @stephack 
- *	 Solution here https://community.hubitat.com/t/parent-child-device-not-actioning-on-off/21694/30?u=royski
+ *   30/08/2019 Updated by @Royski to work with Hubitat, with help from @chuck.schwer and @stephack
+ * 	 Solution here https://community.hubitat.com/t/parent-child-device-not-actioning-on-off/21694/30?u=royski
  *
  *****************************************************************************************************************/
 metadata {
-	definition (name: "GreenWave PowerNode 6 Child Device", namespace: "copycat73", author: "Nick Veenstra") {
-		capability "Switch"
-		capability "Energy Meter"
-		capability "Power Meter"
-		capability "Refresh"
-		
-		attribute "lastupdate", "string"
-		command "reset"
-	}
-	
-	preferences {
-		input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
+    definition(name: "GreenWave PowerNode 6 Child Device", namespace: "copycat73", author: "Nick Veenstra") {
+        capability "Switch"
+        capability "Energy Meter"
+        capability "Power Meter"
+        capability "Refresh"
+
+        attribute "lastupdate", "string"
+        command "reset"
+    }
+
+    preferences {
+        input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
         input name: "txtEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: true
-	}
+    }
 }
 
-def logsOff(){
+def logsOff() {
     log.warn "debug logging disabled..."
-    device.updateSetting("logEnable",[value:"false",type:"bool"])
+    device.updateSetting("logEnable", [value: "false", type: "bool"])
 }
 
 def installed() {
-	log.debug "Greenwave child installed"
+    log.debug "Greenwave child installed"
 }
 
-def updated(){
-log.info "Greenwave child updated"
+def updated() {
+    log.info "Greenwave child updated"
     log.warn "debug logging is: ${logEnable == true}"
     log.warn "description logging is: ${txtEnable == true}"
-    if (logEnable) runIn(1800,logsOff)
-	parent.updateChildLabel(splitChannel(device.deviceNetworkId))
+    if (logEnable) {
+        runIn(1800, logsOff)
+    }
+    parent.updateChildLabel(splitChannel(device.deviceNetworkId))
 }
 
 def on() {
-	if (logEnable) log.debug "$device.label - On"
-	parent.switchOn(splitChannel(device.deviceNetworkId))
+    if (logEnable) {
+        log.debug "$device.label - On"
+    }
+    parent.switchOn(splitChannel(device.deviceNetworkId))
 }
 
 def off() {
-	if (logEnable) log.debug "$device.label - Off"
-	parent.switchOff(splitChannel(device.deviceNetworkId))
+    if (logEnable) {
+        log.debug "$device.label - Off"
+    }
+    parent.switchOff(splitChannel(device.deviceNetworkId))
 }
 
 def refresh() {
-	if (logEnable) log.debug "$device.label - Refresh"
-	parent.pollNode(splitChannel(device.deviceNetworkId))
+    if (logEnable) {
+        log.debug "$device.label - Refresh"
+    }
+    parent.pollNode(splitChannel(device.deviceNetworkId))
 }
 
 def reset() {
-	if (logEnable) log.debug "$device.label - Reset"
-	parent.resetNode(splitChannel(device.deviceNetworkId))
+    if (logEnable) {
+        log.debug "$device.label - Reset"
+    }
+    parent.resetNode(splitChannel(device.deviceNetworkId))
 }
 
-private splitChannel(String channel) {
-    channel.split("-ep") [-1] as Integer
+private static splitChannel(String channel) {
+    channel.split("-ep")[-1] as Integer
 }
